ed6fa17fa12f7c87b10f8d4e1901d8db650089fd
Chris Aprea <1527164+aprea@users.noreply.github.com>
2025-07-20 17:24:01 +1000

    Update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,21 @@
 export const apps = [
 	{
-		name: 'myer-monitor',
+		name: 'myer-monitor-shoes',
 		script: 'index.ts',
+		args: '-q shoes',
+		interpreter: 'bun',
+		env: {
+			NODE_ENV: 'production',
+			PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+		},
+		error_file: './logs/error.log',
+		out_file: './logs/out.log',
+		log_file: './logs/combined.log',
+	},
+	{
+		name: 'myer-monitor-pokemon',
+		script: 'index.ts',
+		args: '-q tcg -f pokemon',
 		interpreter: 'bun',
 		env: {
 			NODE_ENV: 'production',
